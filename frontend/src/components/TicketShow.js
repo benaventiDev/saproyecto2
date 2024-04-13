a5b0542bdf31cc34a5c2cb5acc1ef6a685ac5b65
benaventiDev <201021212@ingenieria.usac.edu.gt>
2024-04-13 16:29:16 -0600

    build and test

--- frontend/src/components/TicketShow.js
+++ frontend/src/components/TicketShow.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import jwt_decode from 'jwt-decode'
 import axios from 'axios'
 import Table from '@material-ui/core/Table';
